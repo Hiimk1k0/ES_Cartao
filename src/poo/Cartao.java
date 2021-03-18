842270dffa101ec7e14c9e47c00dfe51ad916812
Hiimk1k0 <fmfsmgaio@gmail.com>
2021-03-18 18:22:44 +0000

    Adicionado JavaDoc e descricao da classe Cartao

--- src/poo/Cartao.java
+++ src/poo/Cartao.java
@@ -1,4 +1,22 @@
 package poo;
+/**
+ * A classe cartão contem metodos para ver e definir e ver atributos dos funcionários
+ * 
+ * <ul>
+ * <li>getNomeFuncionario</li>
+ * <li>setServico</li>
+ * <li>setCargo</li>
+ * <li>retirarServico</li>
+ * <li>atribuirNovoServico</li>
+ * <li>promoverNovoCargo</li>
+ * <li>despidirFuncionario</li>
+ * </ul>
+ * 
+ * @author Hiimk1k0
+ *
+ */
+
+
 
 public class Cartao {
 	
@@ -20,34 +38,69 @@ public class Cartao {
 	}
 
 	// Acessores
+	/**
+	 * Nome do Funcionario
+	 * 
+	 * @return retorna o nome
+	 */
 	
 	public String getNomeFuncionario() {
 		return nomeFuncionario;
 	}
 
+	/**
+	 * Servico desempenhado pelo funcionario
+	 * 
+	 * @param servico - define qual e o servico realizado
+	 */
+	
 	public void setServico(String servico) {
 		this.servico = servico;
 	}
 
+	/**
+	 * Cargo desempenhado pelo funcionario
+	 * 
+	 * @param cargo - define qual e o cargo ocupado
+	 */
+	
 	public void setCargo(String cargo) {
 		this.cargo = cargo;
 	}
 	
 	// Métodos
+	
+	/**
+	 * Retira o funcionario do servico atual 
+	 */
+	
 	public void retirarServico() {
 		// servico = "";
 		setServico("");
 	}
 	
+	/**
+	 * Atribui um novo servico ao funcionario
+	 * 
+	 * @param novoServico
+	 */
 	public void atribuirNovoServico( String novoServico ) {
 		//servico = novoServico;
 		setServico( novoServico );
 	}
+	/**
+	 * Promove o funcionario a um novo cargo
+	 * 
+	 * @param novoCargo -  novo cargo do funcionario
+	 */
 	
 	public void promoverNovoCargo( String novoCargo ) {
 		//cargo = novoCargo;
 		setCargo(novoCargo);
 	}
+	/**
+	 * Despede o funcionario
+	 */
 	
 	public void despedirFuncionario() {
 		retirarServico();
